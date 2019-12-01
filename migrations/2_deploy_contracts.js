72a82eddd94e2a1a4b1bf2bff2c08c29688d9fb9
CaelRowley <caelrowley@gmail.com>
2019-12-01 15:48:59 +0100

    feat(seeds): new seed data for vaults

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,15 +8,27 @@ module.exports = async (deployer, network) => {
   
   await deployer.deploy.apply(deployer, [ContractRegistry])
   const contractRegistry = await ContractRegistry.deployed()
-  console.log(`Contract Registry Contract address: ${contractRegistry.address}`)
+  console.log(`Contract Registry deploy at address: ${contractRegistry.address}`)
 
   await deployer.deploy.apply(deployer, [VaultManager , contractRegistry.address])
   const vaultManager = await VaultManager.deployed()
-  console.log(`VaultManager Contract address: ${vaultManager.address}`)
+  console.log(`VaultManager Contract deloyed at address: ${vaultManager.address}`)
 
   contractRegistry.addContract('VaultManager', vaultManager.address)
 
-  console.log(`Updated contract manager to contain contracts`)
+  console.log(`Updated ContractRegistry to contain VaultManager`)
+
+  vaultManager.addNewVault('Cat1', 'QmW2WQi7j6c7UgJTarActp7tDNikE4B2qXtFCfLPdsgaTQ')
+  vaultManager.addNewVault('Cat2', 'QmW2WQi7j6c7UgJTarActp7tDNikE4B2qXtFCfLPdsgaTQ')
+  vaultManager.addNewVault('Cat3', 'QmW2WQi7j6c7UgJTarActp7tDNikE4B2qXtFCfLPdsgaTQ')
+  vaultManager.addNewVault('Cat4', 'QmW2WQi7j6c7UgJTarActp7tDNikE4B2qXtFCfLPdsgaTQ')
+  vaultManager.addNewVault('Cat5', 'QmW2WQi7j6c7UgJTarActp7tDNikE4B2qXtFCfLPdsgaTQ')
+
+  vaultManager.addNewVault('Lob1', 'QmW3FgNGeD46kHEryFUw1ftEUqRw254WkKxYeKaouz7DJA')
+  vaultManager.addNewVault('Cat6', 'QmW2WQi7j6c7UgJTarActp7tDNikE4B2qXtFCfLPdsgaTQ')
+  vaultManager.addNewVault('Lob2', 'QmW3FgNGeD46kHEryFUw1ftEUqRw254WkKxYeKaouz7DJA')
+  vaultManager.addNewVault('Cat7', 'QmW2WQi7j6c7UgJTarActp7tDNikE4B2qXtFCfLPdsgaTQ')
+  vaultManager.addNewVault('Lob3', 'QmW3FgNGeD46kHEryFUw1ftEUqRw254WkKxYeKaouz7DJA')
 
   console.log(`Finished deployment on ${network} network`)
 };
